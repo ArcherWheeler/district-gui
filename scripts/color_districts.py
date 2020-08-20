170b455aa800144449045c6c687c3773260415f2
Beenish Valliani <beenish_valliani@brown.edu>
2020-08-19 22:06:35 -0500

    working 6-color alg

--- scripts/color_districts.py
+++ scripts/color_districts.py
@@ -6,25 +6,48 @@ with open('../district_plans/texas.geojson', 'r') as texas_districts_file:
     texas_districts = geojson.load(texas_districts_file)
     texas_districts_file.close()
 
-districts = []
+districts = list()
 
+# Adds districts to list (as Shapely objects)
 for feature in texas_districts["features"]:
     districts.append(shape(feature["geometry"]))
 
 adjacencies = [set() for i in range(len(districts))]
 
+# Creates adjacency list for each district
 for i in range(len(districts)):
     for j in range(i + 1, len(districts)):
         if type(districts[i].intersection(districts[j])) is LineString or type(districts[i].intersection(districts[j])) is MultiLineString:
             adjacencies[i].add(j)
             adjacencies[j].add(i)
 
-coloring = [-1 for i in range(len(districts))]
+degree_list = [list()] * len(districts)
+coloring_order = list()
+coloring = [-1] * len(districts)
 
-# Assign the first vertex with the lowest number.
-coloring[0] = 0
+# Creates list of districts with degree i (for i between 0 and number of districts - 1)
+for i in range(len(districts)):
+    degree_list[len(adjacencies[i])].append(i)
 
-for vertex in range(1, len(coloring)):
+# Establishes order in which districts should be colored to ensure 6-coloring
+for i in range(len(districts)):
+    minimum_degree = next(index for (index, lst) in enumerate(degree_list) if lst)
+    minimum_vertex = degree_list[minimum_degree].pop(0)
+    coloring_order.append(minimum_vertex)
+
+    for neighbor in adjacencies[minimum_vertex]:
+        for index in range(len(degree_list)):
+            if neighbor in degree_list[index]:
+                degree_list[index].remove(neighbor)
+                degree_list[index - 1].append(neighbor)
+                break
+
+# Assign the first district with the lowest number.
+coloring[coloring_order.pop(0)] = 0
+
+# Assign the remaining districts with a number.
+while coloring_order:
+    vertex = coloring_order.pop(0)
     color = 0
     adjacent_colors = set()
     for adjacent in adjacencies[vertex]:
@@ -32,12 +55,10 @@ for vertex in range(1, len(coloring)):
 
     while color in adjacent_colors:
         color += 1
-
+    
     coloring[vertex] = color
 
-available_colors = ["#FF0000", "#FFFF00", "#00EAFF", "#AA00FF", "#4F8F23", "#BFFF00", 
-"#0095FF", "#FF00AA", "#FFD400", "#6AFF00", "#0040FF", "#EDB9B9", "#B9D7ED", "#E7E9B9", 
-"#DCB9ED", "#B9EDE0", "#8F2323", "#23628F", "#8F6A23", "#6B238F", "#FF7F00"]
+available_colors = ["#23628F", "#FF00AA", "#FFD400", "#6AFF00", "#00EAFF", "#6B238F"]
 
 for feature in texas_districts["features"]:
     feature["properties"]["color"] = available_colors[coloring[districts.index(shape(feature["geometry"]))]]
