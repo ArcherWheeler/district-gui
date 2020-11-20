686e1fb1554d84f9457feee1f189fa188a7fd93d
Archer Wheeler <archie.wh@gmail.com>
2020-11-19 19:49:30 -0500

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -euxo pipefail
 
-rm -rf dist
-parcel build --public-url . index.html
+rm -rf docs
+npm run predeploy
 mv dist docs
-echo "redistrictingproject.com" > CNAME
+echo "redistrictingproject.com" > docs/CNAME
