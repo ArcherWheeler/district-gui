60a452ad0dfe13895ed90dabaceb146cfdb8faf3
Beenish Valliani <beenish_valliani@brown.edu>
2021-03-11 18:49:45 -0600

    more map data

--- scripts/clip_districts.py
+++ scripts/clip_districts.py
@@ -5,7 +5,7 @@ from shapely.geometry import shape, mapping
 with open('../state_plans/' + sys.argv[1] + '.geojson', 'r') as state_file:
     state = geojson.load(state_file)
     
-with open('../district_plans/phase_2/' + sys.argv[1] + '.geojson', 'r') as districts_file:
+with open('../pages/district_plans/phase_1/' + sys.argv[1] + '.geojson', 'r') as districts_file:
     districts = geojson.load(districts_file)
     districts_file.close()
     
@@ -15,5 +15,5 @@ for feature in districts["features"]:
     district_polygon = shape(feature["geometry"])
     feature["geometry"] = mapping(state_polygon.intersection(district_polygon))
     
-with open('../district_plans/phase_2/' + sys.argv[1] + '.geojson', 'w') as districts_file:
+with open('../pages/district_plans/phase_1/' + sys.argv[1] + '.geojson', 'w') as districts_file:
     geojson.dump(districts, districts_file)
